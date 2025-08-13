8950c9310f36a06855d2ebcf1b4b1b9ead39ef72
SAHIL RAJA <sahilraja90644112@gmail.com>
2025-08-13 10:03:39 +0530

    Add files via upload

---
+++ Sahil C Language3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    float num1 = 9.6;
+    float num2 = 6.9;
+    float result;
+
+    result = num1 - num2;
+
+    printf("First number : %.1f\n" , num1);
+    printf("second number : %.1f\n" , num2);
+    printf("Result = %.2f\n" , result);
+
+    return 0;
+    
+}
