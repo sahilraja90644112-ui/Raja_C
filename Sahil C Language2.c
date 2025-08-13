295e971b13c05d42817f5c30691d82ac3a8d4eaf
SAHIL RAJA <sahilraja90644112@gmail.com>
2025-08-13 10:03:18 +0530

    Add files via upload

---
+++ Sahil C Language2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+int num1 = 2;
+int num2 = 3;
+int sum;
+
+sum = num1 + num2;
+
+
+printf("First number : %d\n" , num1);
+printf("second number : %d\n" , num2);
+printf("sum = %d\n" , num1);
+
+return 0;
+}
